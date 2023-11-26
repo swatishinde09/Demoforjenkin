b2744132dcb178dcbb44eb7bc0a51ee40979c87f
swatishinde09 <148969831+swatishinde09@users.noreply.github.com>
2023-11-26 12:24:40 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demoforjenkin
+{
+    internal class Program
+    {
+        public void show()
+        {
+            Console.WriteLine("Hello World");
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.show();
+            Console.ReadLine();
+        }
+    }
+}
